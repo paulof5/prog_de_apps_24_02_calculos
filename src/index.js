b91bd43fa21eda31dab1025ae27dfa64d2bd5174
Paulo Fontenele <125470131+paulof5@users.noreply.github.com>
2023-02-24 08:55:33 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let a = 7;
+let b = 3;
+
+b += a;
+console.log(b);
+
+b -= a;
+console.log(b);
+
+b *= a;
+console.log(b);
+
+b /= a;
+console.log(b);
+
+b %= a;
+console.log(b);
+
+b ^= a;
+console.log(b);
+
+b += a;
+console.log(b);
